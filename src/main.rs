d9ed6633e1a34159203479a3260781eba1b82f90
JakubGawron1 <jakubgawron.dev.pl@gmail.com>
2023-10-27 11:00:17 +0200

    Init commit

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,16 @@
 #![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+use uefi::prelude::*;
+
+#[entry]
+fn main(image: Handle, mut system_table: SystemTable<Boot>)-> Status{
+    loop{}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo)-> !{
+    loop{}
+}
